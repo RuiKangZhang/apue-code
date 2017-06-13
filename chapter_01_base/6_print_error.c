18f3ef4852e00cd798ff0141f85880fc0881503f
ruikangzhang <zrk19491001@gmail.com>
2017-06-14 01:33:24 +0800

    print err msg and the meaning of errno
    key notes :
        header :
            #include <errno.h>
        func :
            strerror(int errnum);
            perror(char *msg);

---
+++ chapter_01_base/6_print_error.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("%d\n", EACCES);
+    fprintf(stderr, "EACCES info: %s\n", strerror(EACCES));
+    errno = ENOENT;
+    perror(argv[0]);
+    exit(0);
+}
