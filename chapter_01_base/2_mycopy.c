e996fcefd4f28589f3e9b74a556188a5fa0698d5
amfreezhang <amfreezhang@tencent.com>
2019-01-25 17:22:08 +0800

    Modify: 1.

--- chapter_01_base/2_mycopy.c
+++ chapter_01_base/2_mycopy.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define BUFFSIZE 4096
 
-int main(int argc, char* argv[])
+int main()
 {
     int n;
     char buf[BUFFSIZE];
+    char exit_symbol = '.';  //循环退出标识
 
     while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
     {
-        if (write(STDOUT_FILENO, buf, n) != n) {
-            printf("write error!\n");
+        if (write(STDOUT_FILENO, buf, n) != n)
+        {
+            perror("Error: write to stdout failed!");
             exit(1);
+        }
+
+        //根据输入的最后一个字符判断是否需要退出循环
+        if (buf[n-sizeof("\n")] == exit_symbol)
+        {
+            printf("Exit program....\n");
+            break;
         }
     }
 
-    if (n < 0) {
-        printf("read error!\n");
+    if (n < 0)
+    {
+        perror("Error: read from stdin failed!");
+        exit(1);
     }
 
-    exit(0);
+    return 0;
 }
--- chapter_01_base/8_exec_cmd_signal.c
+++ chapter_01_base/8_exec_cmd_signal.c
@@ -1,1 +1,23 @@
 //waiting for achieving
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+// 信号处理函数
+static void sig_interupt(int signo)
+{
+	printf("Interupt!\n%% ");
+}
+
+int main(int argc, char *argv[])
+{
+	// 扑捉 SIGINT 信号并交由信号处理函数处理
+    if (signal(SIGINT, sig_interupt) == SIG_ERR)
+    {
+
+    }  
+
+
+	return 0;
+}
